e1d4891fb94d165acc909d7002d582379b1a8bc3
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-17 23:23:19 +0545

    Add model order

---
+++ models/order.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const orderSchema = mongoose.Schema({
+    productId: {
+        type: mongoose.ObjectId,
+        required:true
+    },
+    userId: {
+        type: mongoose.ObjectId,
+        required:true
+    },
+    quantity: {
+        type: Number,
+        required:true
+    },
+    status:{
+        type: String,
+        default: "Pending",
+        require: true,
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        require: true
+    }
+});
+
+orderSchema.plugin(uniqueValidator);
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
