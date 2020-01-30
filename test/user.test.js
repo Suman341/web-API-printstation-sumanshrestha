19912ab37782a15b5e9b381e9c604c45fe316cb2
Suman341 <55776175+Suman341@users.noreply.github.com>
2020-01-30 22:02:27 +0545

    all user test

--- test/user.test.js
+++ test/user.test.js
@@ -23,23 +23,53 @@ afterAll(async () => {
 
 
 describe('User Schema testing', () => {
+    // the code below is for insert testing
+        // it('Register User testing', () => {
+        //     const user = {
+        //         'firstname': 'Suman',
+        //         'lastname': 'Shresta',
+        //         'address': 'Kathmandu',
+        //         'phone': '9808548545',
+        //         'email': 'suman@gmail.com',
+        //         'password': '1234'
+        //     };
+
+        //     return User.create(user)
+        //         .then((pro_ret) => {
+        //             expect(pro_ret.email).toEqual('suman@gmail.com');
+        //         });
+        // });
 
 
-    to update user details by id
-     it('to test update user', async () => {
-
-         return User.findOneAndUpdate({_id :Object('5e47d4ac01c2570d08cb98fd')}, {$set : {firstname:'sujan'}})
-         .then((pp)=>{
-             expect(pp.firstname).toEqual('sujan')
+     delete individual user by id
+         it('to test delete a user', async () => {
+             const status = await User.deleteOne({_id :Object('5e47d4ac01c2570d08cb98fd')});
+             expect(status.ok).toBe(1);
          })
 
-     });
 
-      select all user
-      it('testing the user for select all at once', async () => {
-         const status = await User.find({});
-         expect(status.length).toBeGreaterThan(0);
-     })
+        //delete all user
+        // it('to test the delete all user ', async () => {
+        //     const status = await User.deleteMany();
+        //     expect(status.ok).toBe(1);
+        // })
+
+
+    //to update user details by id
+    // it('to test update user', async () => {
+
+    //     return User.findOneAndUpdate({_id :Object('5e47d4ac01c2570d08cb98fd')}, {$set : {firstname:'sujan'}})
+    //     .then((pp)=>{
+    //         expect(pp.firstname).toEqual('sujan')
+    //     })
+
+    // });
+
+     // select all user
+    //  it('testing the user for select all at once', async () => {
+    //     const status = await User.find({});
+    //     expect(status.length).toBeGreaterThan(0);
+    // })
 
 
     
